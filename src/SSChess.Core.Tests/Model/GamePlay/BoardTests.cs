4e6d948fd131e6aeadccb256309dc69614184dc1
Stan Sajous <stan.sajous@gmail.com>
2013-02-02 20:36:09 -0600

    rearranged namepaces

---
+++ src/SSChess.Core.Tests/Model/GamePlay/BoardTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSChess.Core.Tests
+{
+    [TestFixture]
+    public class BoardTests
+    {
+    }
+}
