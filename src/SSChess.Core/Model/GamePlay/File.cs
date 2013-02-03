57177f861a7c21558fed18f6765fa2b786cdcc02
Stan Sajous <stan.sajous@gmail.com>
2013-02-02 20:43:12 -0600

    Added File class

---
+++ src/SSChess.Core/Model/GamePlay/File.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSChess.Core.Model.GamePlay
+{
+    public class File
+    {
+        public const int MaxIndex = 8;
+        public const int MinIndex = 1;
+        public const char MaxName = 'h';
+        public const char MinName = 'a';
+
+        public char Name 
+        { 
+            get; 
+            set; 
+        }
+        public int Index 
+        { 
+            get; 
+            set; 
+        }
+
+        public File(int index)
+        {
+
+        }
+
+        public File(char name)
+        {
+
+        }
+    }
+}
