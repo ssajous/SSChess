3e1ba4790752b063bb257f33384778463b96bdba
Stan Sajous <stan.sajous@gmail.com>
2013-02-04 22:05:19 -0600

    Setup of initial board

--- src/SSChess.Core/Model/GamePlay/Board.cs
+++ src/SSChess.Core/Model/GamePlay/Board.cs
@@ -34,7 +34,97 @@ namespace SSChess.Core.Model.GamePlay
 
         public void SetupStartingPieces()
         {
-            throw new NotImplementedException();
+            SetupWhitePieces();
+            SetupBlackPieces();
+        }
+
+        private void AddPieceToBoard(Piece piece, Position square)
+        {
+            piece.Board = this;
+            piece.Square = square;
+            Pieces.Add(piece);
+        }
+
+        private void SetupWhitePieces() 
+        {
+            Piece currentPiece;
+
+            CreatePawnAtPosition(StartingPositions.WHITE_A_PAWN);
+            CreatePawnAtPosition(StartingPositions.WHITE_B_PAWN);
+            CreatePawnAtPosition(StartingPositions.WHITE_C_PAWN);
+            CreatePawnAtPosition(StartingPositions.WHITE_D_PAWN);
+            CreatePawnAtPosition(StartingPositions.WHITE_E_PAWN);
+            CreatePawnAtPosition(StartingPositions.WHITE_F_PAWN);
+            CreatePawnAtPosition(StartingPositions.WHITE_G_PAWN);
+            CreatePawnAtPosition(StartingPositions.WHITE_H_PAWN);
+
+            // Rooks
+            currentPiece = new Rook();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.WHITE_A_ROOK));
+            currentPiece = new Rook();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.WHITE_H_ROOK));
+
+            // Knights
+            currentPiece = new Knight();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.WHITE_B_KNIGHT));
+            currentPiece = new Knight();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.WHITE_G_KNIGHT));
+
+            // Bishops
+            currentPiece = new Bishop();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.WHITE_LIGHT_BISHOP));
+            currentPiece = new Bishop();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.WHITE_DARK_BISHOP));
+
+            // Royalty
+            currentPiece = new King();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.WHITE_KING));
+            currentPiece = new Queen();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.WHITE_QUEEN));
+        }
+
+        private void SetupBlackPieces()
+        {
+            Piece currentPiece;
+
+            CreatePawnAtPosition(StartingPositions.BLACK_A_PAWN);
+            CreatePawnAtPosition(StartingPositions.BLACK_B_PAWN);
+            CreatePawnAtPosition(StartingPositions.BLACK_C_PAWN);
+            CreatePawnAtPosition(StartingPositions.BLACK_D_PAWN);
+            CreatePawnAtPosition(StartingPositions.BLACK_E_PAWN);
+            CreatePawnAtPosition(StartingPositions.BLACK_F_PAWN);
+            CreatePawnAtPosition(StartingPositions.BLACK_G_PAWN);
+            CreatePawnAtPosition(StartingPositions.BLACK_H_PAWN);
+
+            // Rooks
+            currentPiece = new Rook();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.BLACK_A_ROOK));
+            currentPiece = new Rook();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.BLACK_H_ROOK));
+
+            // Knights
+            currentPiece = new Knight();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.BLACK_B_KNIGHT));
+            currentPiece = new Knight();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.BLACK_G_KNIGHT));
+
+            // Bishops
+            currentPiece = new Bishop();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.BLACK_LIGHT_BISHOP));
+            currentPiece = new Bishop();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.BLACK_DARK_BISHOP));
+
+            // Royalty
+            currentPiece = new King();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.BLACK_KING));
+            currentPiece = new Queen();
+            AddPieceToBoard(currentPiece, new Position(StartingPositions.BLACK_QUEEN));
+        }
+
+        private void CreatePawnAtPosition(string position)
+        {
+            Pawn pawn = new Pawn();
+            AddPieceToBoard(pawn, new Position(position));
         }
     }
 }
